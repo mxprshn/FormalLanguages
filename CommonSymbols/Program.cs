2dcc7cfb80a52d1da848f8bb5064e124be7aefc9
Max Parshin <maksim.parshin@lanit-tercom.com>
2020-09-21 23:57:41 +0300

    Add common symbols task

---
+++ CommonSymbols/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace CommonSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            var firstText = "";
+            var secondText = "";
+
+            if (args.Length == 2 && !File.Exists(args[0]) && !File.Exists(args[1]))
+            {
+                firstText = File.ReadAllText(args[0]);
+                secondText = File.ReadAllText(args[1]);
+            }
+            else
+            {
+                Console.Write("Введите первый текст: ");
+                firstText = Console.ReadLine();
+
+                Console.Write("Введите второй текст: ");
+                secondText = Console.ReadLine();
+            }
+
+            var firstTextSymbols = new HashSet<char>();
+            var secondTextSymbols = new HashSet<char>();
+
+            foreach (var symbol in firstText)
+            {
+                firstTextSymbols.Add(symbol);
+            }
+
+            foreach (var symbol in secondText)
+            {
+                secondTextSymbols.Add(symbol);
+            }
+
+            firstTextSymbols.IntersectWith(secondTextSymbols);
+
+            Console.WriteLine("Общие символы в текстах:");
+
+            foreach (var symbol in firstTextSymbols)
+            {
+                Console.WriteLine($"{symbol}");
+            }
+        }
+    }
+}
