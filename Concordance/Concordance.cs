44250c90e5ae6eebb1c778c33930f961b30995c6
Max Parshin <maksim.parshin@lanit-tercom.com>
2020-09-24 16:34:02 +0300

    Add concordance

---
+++ Concordance/Concordance.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Concordance
+{
+    public class Concordance
+    {
+        private static HashSet<char> wordConnectors = new HashSet<char> { '\'', '-', '’' };
+
+        private Dictionary<string, SortedSet<int>> wordMap = new Dictionary<string, SortedSet<int>>();
+
+        public bool AddWord(string word, int lineNumber)
+        {
+            if (!wordMap.ContainsKey(word))
+            {
+                wordMap.Add(word, new SortedSet<int>());
+            }
+
+            return wordMap[word].Add(lineNumber);
+        }
+
+        public void ReadText(string text, bool removeWordConnectors)
+        {
+            var lines = text.ToLower().Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+            for (var i = 0; i < lines.Length; ++i)
+            {
+                var words = new string(lines[i].Select(c => (removeWordConnectors && wordConnectors.Contains(c)) ||
+                    (char.IsPunctuation(c) && !wordConnectors.Contains(c)) ? ' ' : c).ToArray())
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var word in words)
+                {
+                    AddWord(word, i + 1);
+                }
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Алфавитный указатель:\n");
+            foreach (var word in wordMap.OrderBy(w => w.Key))
+            {
+                Console.Write($"{word.Key + ":",15}");
+
+                foreach (var lineNumber in word.Value)
+                {
+                    Console.Write($"{lineNumber,5}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Concordance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Concordance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            if (args.Length < 1 || !File.Exists(args[0]))
+            {
+                Console.WriteLine("Путь к файлу не указан или файл не найден.");
+                return;
+            }
+
+            var removeWordConnectors = false;
+
+            if (args.Length == 2 && args[1] == "-r")
+            {
+                removeWordConnectors = true;
+            }
+
+            var source = File.ReadAllText(args[0]);
+
+            var concordance = new Concordance();
+            concordance.ReadText(source, removeWordConnectors);
+
+            Console.WriteLine($"Исходный текст:\n\n{source}\n");
+
+            concordance.Print();
+        }
+    }
+}
