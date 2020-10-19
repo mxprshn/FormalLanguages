b2918b9b2a5c23842faf9c9d830e98cf99aca15c
Max Parshin <maksim.parshin@lanit-tercom.com>
2020-10-19 13:04:47 +0300

    Add task about squares

---
+++ Squares/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Squares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.Write("Введите размер квадрата: ");
+            var n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 1; i <= n; ++i)
+            {
+                for (int j = 1; j <= n; ++j)
+                {
+                    sum += (n - i + 1) * (n - j + 1);
+                }
+            }
+
+            Console.WriteLine($"Ответ: {sum}");
+        }
+    }
+}
